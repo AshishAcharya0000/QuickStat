3384f2e65a31aec1e46549c180c9abf9afbf2440
Ashish <ashishacharya0@gmail.com>
2022-03-22 00:02:07 -0600

    removed time/clocks/show_source_code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
-from time import time
 from kivy.app import App
 from os.path import dirname, join
 from kivy.lang import Builder
 from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
     ListProperty
-from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
 
 
@@ -22,21 +20,18 @@ class ShowcaseApp(App):
 
     index = NumericProperty(-1)
     current_title = StringProperty()
-    time = NumericProperty(0)
-    show_sourcecode = BooleanProperty(False)
-    sourcecode = StringProperty()
     screen_names = ListProperty([])
     hierarchy = ListProperty([])
 
     def build(self):
         self.title = 'QuickStat'
-        Clock.schedule_interval(self._update_clock, 1 / 60.)
+        
         self.screens = {}
         self.available_screens = [
             'FileChoosers',
             'Buttons', 'ToggleButton', 'Sliders', 'ProgressBar',
             'Switches', 'CheckBoxes', 'TextInputs', 'Accordions', 
-            'Carousel', 'Bubbles', 'CodeInput', 'DropDown', 
+            'Carousel', 'Bubbles', 'DropDown', 
             'Spinner', 'Scatter', 'Splitter', 'TabbedPanel + Layouts', 
             'RstDocument', 'Popups', 'ScreenManager']
         self.screen_names = self.available_screens
@@ -84,7 +79,7 @@ class ShowcaseApp(App):
         return screen
 
     def showcase_floatlayout(self, layout):
-        print("test") 
+        print("test")
 
     def showcase_boxlayout(self, layout):
         print('test')
@@ -98,8 +93,7 @@ class ShowcaseApp(App):
     def showcase_anchorlayout(self, layout):
         print('test')
 
-    def _update_clock(self, dt):
-        self.time = time()
+    
 
 
 if __name__ == '__main__':
